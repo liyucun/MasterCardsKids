08c2507ddc4affa4151f87170ee2d384f91ec4ee
yucun li <liyucun2012@gmail.com>
2015-09-27 12:01:18 -0400

    home fragment final

--- MyApplication/app/src/main/java/com/yucun/mastercardsforkids/fragment/HomeFragment.java
+++ MyApplication/app/src/main/java/com/yucun/mastercardsforkids/fragment/HomeFragment.java
@@ -71,12 +71,15 @@ public class HomeFragment extends Fragment {
         scan_text.setTypeface(font);
 
         task_button.setButtonColor(getResources().getColor(R.color.brown_500));
+        task_button.setImageResource(R.drawable.abc_btn_rating_star_on_mtrl_alpha);
         wallet_button.setButtonColor(getResources().getColor(R.color.brown_500));
+        wallet_button.setImageResource(R.drawable.abc_btn_rating_star_on_mtrl_alpha);
         goal_button.setButtonColor(getResources().getColor(R.color.brown_500));
+        goal_button.setImageResource(R.drawable.abc_btn_rating_star_on_mtrl_alpha);
         transaction_button.setButtonColor(getResources().getColor(R.color.brown_500));
+        transaction_button.setImageResource(R.drawable.abc_btn_rating_star_on_mtrl_alpha);
         scan_button.setButtonColor(getResources().getColor(R.color.brown_500));
-
-        task_button.playShowAnimation();
+        scan_button.setButtonColor(getResources().getColor(R.color.brown_500));
 
         return view;
     }
@@ -86,6 +89,16 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //((MainActivity)getActivity()).getProfile()
         taskArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.container, TaskFragment.instantiate(getActivity(), TaskFragment.class.getName()))
+                        .addToBackStack("home")
+                        .commit();
+            }
+        });
+        task_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getFragmentManager()
@@ -105,7 +118,27 @@ public class HomeFragment extends Fragment {
                         .commit();
             }
         });
+        wallet_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.container, WalletFragment.instantiate(getActivity(), WalletFragment.class.getName()))
+                        .addToBackStack("home")
+                        .commit();
+            }
+        });
         goalsArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.container, GoalsFragment.instantiate(getActivity(), GoalsFragment.class.getName()))
+                        .addToBackStack("home")
+                        .commit();
+            }
+        });
+        goal_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getFragmentManager()
@@ -125,11 +158,38 @@ public class HomeFragment extends Fragment {
                         .commit();
             }
         });
+        transaction_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.container, TransactionFragment.instantiate(getActivity(), TransactionFragment.class.getName()))
+                        .addToBackStack("home")
+                        .commit();
+            }
+        });
         scan_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), EarnMoneyActivity.class));
             }
         });
+        scan_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), EarnMoneyActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        task_button.playShowAnimation();
+        wallet_button.playShowAnimation();
+        goal_button.playShowAnimation();
+        transaction_button.playShowAnimation();
+        scan_button.playShowAnimation();
     }
 }
