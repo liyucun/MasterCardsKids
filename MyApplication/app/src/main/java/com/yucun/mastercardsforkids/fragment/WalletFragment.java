0014d0ea484e4ef12f66b2ddfdb8f1e6b0aa8f03
Jianhui Zhu <jianhuizhu1987@gmail.com>
2015-09-27 01:12:42 -0400

    fragment function done

--- MyApplication/app/src/main/java/com/yucun/mastercardsforkids/fragment/WalletFragment.java
+++ MyApplication/app/src/main/java/com/yucun/mastercardsforkids/fragment/WalletFragment.java
@@ -1,9 +1,39 @@
 package com.yucun.mastercardsforkids.fragment;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.yucun.mastercardsforkids.R;
+import com.yucun.mastercardsforkids.activity.MainActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by jianhuizhu on 15-09-26.
  */
 public class WalletFragment extends Fragment {
+    @Bind(R.id.allowance_record)
+    TextView allowanceRecord;
+    @Bind(R.id.educash_record)
+    TextView educashRecord;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.wallet_fragment,container,false);
+        ButterKnife.bind(this,view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        allowanceRecord.setText(Integer.toString(((MainActivity) getActivity()).getProfile().getAllowance()));
+        educashRecord.setText(Integer.toString(((MainActivity) getActivity()).getProfile().getEducash()));
+    }
 }
